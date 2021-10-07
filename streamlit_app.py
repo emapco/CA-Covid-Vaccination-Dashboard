abaac1a203d23beb5f9d997eb1405466b5d7be37
Emmanuel Cortes <polosetroc@gmail.com>
2021-10-06 23:09:16 -0700

    Updated content

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,9 @@ from apps import demographics, state, county  # import your app modules here
 app = MultiApp()
 
 # Adding applications
+app.add_app("County Data", county.app)
 app.add_app("Demographic Data", demographics.app)
 app.add_app("State Data", state.app)
-app.add_app("State Map with County Data", county.app)
 
 # The main app
 app.run()
